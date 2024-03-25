eb127a1d7a71699fb70d9cd9a210e7b2cf22cd95
felipepalage <fguilherme323@gmail.com>
2024-03-24 22:50:45 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraSimples/Controllers/CalculadoraController.cs
@@ -0,0 +1,51 @@
+﻿using CalculadoraSimples.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraSimples.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculadoraController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Calculadora> somar(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1,valor2);
+            calc.Valor1 = valor1;
+            calc.Valor2 = valor2;
+            calc.somar();
+            return Ok(valor1 + valor2);
+        }
+
+        [HttpGet]
+        public ActionResult<Calculadora> Subtrair(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.Valor1 = valor1;
+            calc.Valor2 = valor2;
+            calc.subtrair();
+            return Ok(valor1 + valor2);
+        }
+
+        [HttpGet]
+        public ActionResult<Calculadora> Multiplicar(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.Valor1 = valor1;
+            calc.Valor2 = valor2;
+            calc.multiplicar();
+            return Ok(valor1 + valor2);
+        }
+
+        [HttpGet]
+        public ActionResult<Calculadora>dividir(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.Valor1 = valor1;
+            calc.Valor2 = valor2;
+            calc.dividir();
+            return Ok(valor1 + valor2);
+        }
+    }
+}
